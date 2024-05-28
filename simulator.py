ee42c9a5fef131333e6302258e4369888651465c
Michelle Li <mwl9639@nyu.edu>
2024-05-28 13:02:36 -0400

    Modifications to calculations v0, deltat, angle, and graphing

--- simulator.py
+++ simulator.py
@@ -3,10 +3,11 @@ import numpy as np
 import random
 from scipy.stats import norm
 
-# Input parameters,
+# Input parameters
 N = 100  # number of turns
-T = 600  # from Ch16, there are at least 100 turns for ~10 min of experiment
-time_step = 1 # in seconds
+T = 600  # total time in seconds
+time_step = 1  # in seconds
+total_time = 0
 
 turning_probability = (N / T) / time_step
 
@@ -31,25 +32,32 @@ movement_sequence = []
 for _ in np.arange(0, int(T), time_step):
     turn_or_not = np.random.uniform(0.0, 1.0)  # random float to compare with probability of turning
 
-    v0 = np.random.normal(loc=2.9095, scale=0.7094, size=1)[0] * 0.2645833333 # speed of larva in px/s convert -> mm/s
+    v0 = np.random.normal(loc=2.9095, scale=0.7094, size=1)[0]  # speed of larva in px/s
     deltat = np.random.normal(loc=18.704, scale=23.316, size=1)[0]  # change in time between turns
+    total_time += deltat
 
     if turn_or_not < turning_probability:  # will turn either left or right
+        # First, move to the current position based on the previous angle
+        x += v0 * np.cos(np.radians(angle)) * time_step
+        y += v0 * np.sin(np.radians(angle)) * time_step
+        
+        # Record turn point
+        turn_points_x.append(x)
+        turn_points_y.append(y)
+
+        # Determine direction of turn
         prob_left_right = 0.5  # probability of left or right is 1/2 or 5/10
         left_or_right = random.random()  # random number to compare with probability of going left or right
 
-        # angle at which larva will turn wrt the direction it's already facing
-        reference_angle = np.random.normal(loc=66.501, scale=36.874, size=1)[0] # angle in degrees
+        # Angle at which larva will turn wrt the direction it's already facing
+        reference_angle = np.random.normal(loc=66.501, scale=36.874, size=1)[0]  # angle in degrees
 
         if left_or_right < prob_left_right:  # if random number is <0.5, go left
             angle += reference_angle  # turn left by reference angle
         else:  # if random number is >=0.5, go right
             angle -= reference_angle  # turn right by reference angle
+        
         num_turns += 1
-
-        # Record turn point
-        turn_points_x.append(x)
-        turn_points_y.append(y)
 
         # Record movement type
         movement_sequence.append('T')
@@ -57,19 +65,17 @@ for _ in np.arange(0, int(T), time_step):
         num_runs += 1
         # Record movement type
         movement_sequence.append('S')
-
-    # Update position
-    x += v0 * np.cos(angle) * deltat  # move in x direction
-    y += v0 * np.sin(angle) * deltat  # move in y direction
+        
+        # Update position without turning
+        x += v0 * np.cos(np.radians(angle)) * time_step
+        y += v0 * np.sin(np.radians(angle)) * time_step
 
     # Append new position to the lists
     x_positions.append(x)
     y_positions.append(y)
 
-
 print("Number of turns:", num_turns)
 print("Number of straight runs:", num_runs)
-
 
 # Runs Test
 def runs_test(sequence):
@@ -101,7 +107,6 @@ def runs_test(sequence):
 
     return runs, expected_runs, z, p_value
 
-
 runs, expected_runs, z, p_value = runs_test(movement_sequence)
 alpha = 0.05  # Significance level
 
@@ -118,17 +123,19 @@ if runs is not None:
 else:
     print("Not enough data to perform the Runs Test")
 
+# Print the amount of time the runs took
+print(f"Total time taken: {total_time} seconds")
+
 
 # Plot trajectory
-plt.scatter(0, 0, color='green', label='Starting Point')  # Plot starting position
+plt.scatter(0, 0, color='green', label='Start Position')  # Plot starting position
 plt.plot(x_positions, y_positions, label='Trajectory')
-plt.scatter(turn_points_x, turn_points_y, color='red', label='Turn Points')  # Plot turn points as red dots
-plt.title('Larva Trajectory with Turn Points')
+plt.scatter(turn_points_x, turn_points_y, color='red', label='Turning Points')  # Plot turn points as red dots
+plt.scatter(x_positions[-1], y_positions[-1], color='blue', label='End Position')  # Plot last point in trajectory
+plt.title('Larva Random Walk with Turning Points')
 plt.xlabel('X position')
 plt.ylabel('Y position')
-#plt.xlim(-100, 100)  # Set x-axis limits
-#plt.ylim(-100, 100)  # Set y-axis limits
 plt.legend()
 plt.grid(True)
-plt.show(block=True)
+plt.show()
 
