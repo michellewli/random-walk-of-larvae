5280dcccf95408514fb5bf76b9828b5e21f5200d
Michelle Li <mwl9639@nyu.edu>
2024-06-11 13:05:18 -0400

    modified code so that it shows a histogram of turn times as well.. just to verify distribution

--- multi_simulator.py
+++ multi_simulator.py
@@ -35,7 +35,7 @@ class LarvaWalker:
     def simulate(self):
         elapsed_time = 0
         timestamp = 0
-        while elapsed_time <= self.T:
+        while timestamp <= self.T:
             elapsed_time += self.time_step
             timestamp += self.time_step
             turn_or_not = np.random.uniform(0.0, 1.0)  # random float to compare with probability of turning
@@ -180,27 +180,26 @@ def main():
     interactive_filename = f'images/larva_path_{timestamp}.html'
     mpld3.save_html(plt.gcf(), interactive_filename)
 
-    plt.show()
-
     # Collect all turn_time values
     all_turn_times = []
     for walker in walkers:
         all_turn_times.extend(walker.turn_times)
 
     # Plot histogram of turn_time values
-    fig, axs = plt.figure(figsize=(8, 6))
-    plt.hist(all_turn_times, bins=30, color='orange', edgecolor='black', alpha=0.7)
-    plt.title('Histogram of Turn Times')
-    plt.xlabel('Turn Time (seconds)')
-    plt.ylabel('Frequency')
-    plt.grid(True)
+    fig, axs = plt.subplots(figsize=(8, 6))
+    axs.hist(all_turn_times, bins=30, color='orange', edgecolor='black', alpha=0.7)
+    axs.set_title('Histogram of Turn Times')
+    axs.set_xlabel('Turn Time (seconds)')
+    axs.set_ylabel('Frequency')
+    axs.grid(True)
     plt.show()
-    # Save histograms as HTML using mpld3
+
+    # Save histogram as HTML using mpld3
     hist_interactive_filename = f'histograms/turn_time_histogram_{timestamp}.html'
     mpld3.save_html(fig, hist_interactive_filename)
 
     # Plot histograms of all speeds and angles
-    fig, axs = plt.subplots(1, 2, figsize=(6, 4))
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
 
     axs[0].hist(all_speeds, bins=30, color='blue', edgecolor='black', alpha=0.7)
     axs[0].set_title('Histogram of Speeds')
@@ -211,10 +210,6 @@ def main():
     axs[1].set_title('Histogram of Angles')
     axs[1].set_xlabel('Angle (radians)')
     axs[1].set_ylabel('Frequency')
-
-    # Save histograms as HTML using mpld3
-    hist_interactive_filename = f'histograms/larva_histograms_{timestamp}.html'
-    mpld3.save_html(fig, hist_interactive_filename)
 
     plt.show()
 
